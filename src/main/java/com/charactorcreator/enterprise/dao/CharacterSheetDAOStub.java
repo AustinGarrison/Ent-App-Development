a0ab9bb15bf169f60eac557afd1b6c04c97334e2
M-Bischoff <93992306+M-Bischoff@users.noreply.github.com>
2022-02-23 09:36:54 -0500

    Added CharacterSheetDAOStub that implements ICharacterSheetDAO. Includes a private HashMap that stores all characterSheets. CharacterId as the key and characterSheet as the value. Implements save method from ICharacterSheetDAO using HashMap.put() to add character sheet to the private HashMap.

---
+++ src/main/java/com/charactorcreator/enterprise/dao/CharacterSheetDAOStub.java
@@ -0,0 +1,17 @@
+package com.charactorcreator.enterprise.dao;
+
+import com.charactorcreator.enterprise.dto.CharacterSheet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharacterSheetDAOStub implements ICharacterSheetDAO{
+    private Map<Integer, CharacterSheet> allCharacterSheets = new HashMap<>();
+
+    @Override
+    public CharacterSheet save(CharacterSheet characterSheet) throws Exception {
+        Integer id = characterSheet.getId();
+        allCharacterSheets.put(id, characterSheet);
+        return characterSheet;
+    }
+}
