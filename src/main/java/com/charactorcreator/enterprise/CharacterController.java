e0423f646dd4da93b0647df11de2c72dafda5b75
Austin Garrison <garrisonaustin95@gmail.com>
2022-03-03 16:42:33 -0500

    Implemented Code Reviews

--- src/main/java/com/charactorcreator/enterprise/CharacterController.java
+++ src/main/java/com/charactorcreator/enterprise/CharacterController.java
@@ -21,21 +21,59 @@ public class CharacterController {
         return "index";
     }
 
+    /**
+     * Get all characters.
+     *
+     * Returns one of the following status codes:
+     * 200: characters found
+     *
+     * @return all created characters
+     */
     @GetMapping("/character")
     public ResponseEntity getAllCharacters() {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    /**
+     * Get character with a given id.
+     *
+     * Given the parameter id, find an item that corresponds to this unique id.
+     *
+     * Returns one of the following status codes:
+     * 200: character found
+     * @param id
+     * @return character with the given id
+     */
     @GetMapping("/character/{id}/")
     public ResponseEntity getCharacterByID(@PathVariable("id") String id) {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    /**
+     * Create a new character
+     *
+     * Creates a json with the information necessary to create a new character
+     *
+     * Returns one of the following status codes:
+     * 200: character created
+     * @param character
+     * @return a json with character information
+     */
     @PostMapping(value = "/character", consumes = "application/json", produces = "application/json")
     public CharacterSheet createCharacter(@RequestBody CharacterSheet character){
         return character;
     }
 
+    /**
+     * Delete character
+     *
+     * Given parameter id, delete character associated with that id
+     *
+     * Returns one of the following status codes:
+     * 200: character created
+     * @param id
+     * @return
+     */
     @DeleteMapping("/character/{id}/")
     public ResponseEntity deleteCharacter(@PathVariable("id") String id) {
         return new ResponseEntity(HttpStatus.OK);
