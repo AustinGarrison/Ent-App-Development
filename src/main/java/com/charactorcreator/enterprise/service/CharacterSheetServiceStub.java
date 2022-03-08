8142509f60dcc3f4d22d81ebee9b0e34395e49b8
Johnathan Otten <ottenjg@mail.uc.edu>
2022-03-08 16:37:23 -0500

    Updated service classes

--- src/main/java/com/charactorcreator/enterprise/service/CharacterSheetServiceStub.java
+++ src/main/java/com/charactorcreator/enterprise/service/CharacterSheetServiceStub.java
@@ -3,7 +3,10 @@ package com.charactorcreator.enterprise.service;
 
 import com.charactorcreator.enterprise.dao.ICharacterSheetDAO;
 import com.charactorcreator.enterprise.dto.CharacterSheet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class CharacterSheetServiceStub implements ICharacterSheetService{
@@ -19,7 +22,7 @@ public class CharacterSheetServiceStub implements ICharacterSheetService{
     }
 
     @Override
-    public CharacterSheet getByID(int id) {
+    public CharacterSheet fetchByID(int id) {
         CharacterSheet characterSheet = new CharacterSheet();
         characterSheet.setStrength(14);
         characterSheet.setId(1);
@@ -30,5 +33,10 @@ public class CharacterSheetServiceStub implements ICharacterSheetService{
     @Override
     public CharacterSheet save(CharacterSheet characterSheet) throws Exception {
         return characterSheetDAO.save(characterSheet);
+    }
+
+    @Override
+    public List<CharacterSheet> fetchAll() {
+        return characterSheetDAO.fetchAll();
     }
 }
